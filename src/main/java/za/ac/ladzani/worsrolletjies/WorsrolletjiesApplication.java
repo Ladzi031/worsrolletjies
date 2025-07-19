a27f4c97ce89c7eaddbbff6a2ec8c469848df58c
ladzi031 <ladziladzani@gmail.com>
2025-07-19 14:47:16 +0200

    first commit

---
+++ src/main/java/za/ac/ladzani/worsrolletjies/WorsrolletjiesApplication.java
@@ -0,0 +1,13 @@
+package za.ac.ladzani.worsrolletjies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorsrolletjiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorsrolletjiesApplication.class, args);
+	}
+
+}
